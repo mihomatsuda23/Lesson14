5b21b65af39baf528b75bf1dfe1f3f19c8760cc0
miho.matsuda <pearl1walk@gmail.com>
2021-02-25 17:13:25 +0900

    Add Employee create

--- src/controllers/employees/EmployeesNewServlet.java
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -16,7 +16,7 @@ import models.Employee;
  */
 @WebServlet("/employees/new")
 public class EmployeesNewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,15 +25,15 @@ public class EmployeesNewServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("_token", request.getSession().getId());
-		request.setAttribute("employee", new Employee());
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("employee", new Employee());
 
-		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
-		rd.forward(request, response);
-	}
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+        rd.forward(request, response);
+    }
 
 }
